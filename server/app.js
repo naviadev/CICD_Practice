27b482778f978e1961cc145f7646dc39cd1be7b6
naviadev <qo2oq1020@gmail.com>
2024-12-06 14:54:42 +0900

    Rel | Release 1.1.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,26 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors'); // CORS 패키지 추가
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("안녕");
+// CORS 설정
+const allowedOrigins = ['https://testpractice1020.s3.ap-northeast-2.amazonaws.com'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // 허용된 요청이면 통과
+    } else {
+      callback(new Error('Not allowed by CORS')); // 허용되지 않은 요청이면 에러 반환
+    }
+  }
+}));
+
+// 기본 라우트
+app.get('/', (req, res) => {
+  res.send('안녕');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// 서버 실행
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
